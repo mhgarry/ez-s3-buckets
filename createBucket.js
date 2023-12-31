5a8cf2a0e9c0030a93e99e1777ec310e00ca6f65
mhgarry <mhgarry92@gmail.com>
2023-12-31 02:23:23 -0500

    create bucket functionality

--- createBucket.js
+++ createBucket.js
@@ -1,43 +1,45 @@
-/*
-	script to create a bucket if you don't already
-	have an existing bucket to populate with your data.
- */
+require('dotenv').config();
 const AWS = require('aws-sdk');
 
-require('dotenv').config;
+// Set AWS credentials from environment variables if provided
+const awsConfig = { region: 'us-east-1' };
+if (process.env.AWS_ACCESS_KEY && process.env.AWS_SECRET_KEY) {
+  awsConfig.accessKeyId = process.env.AWS_ACCESS_KEY;
+  awsConfig.secretAccessKey = process.env.AWS_SECRET_KEY;
+}
+AWS.config.update(awsConfig);
 
-//set your region, I'm using us-east-1, but you can use any region you prefer
+// Create a new S3 service object
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
-/* if you are using the aws-cli you don't need to specify anything, region is there as a default, if you aren't using the aws-cli you need to specify region, default key, and secret key */
-AWS.config.update({
-  region: 'us-east-1',
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretKey: process.env.AWS_SECRET_KEY,
-});
+// Define the name for your bucket
+const bucketName = 'my-unique-bucket-name'; // Ensure this is unique
 
-// create a new S3 service object with specified parameters
-const s3 = new AWS.S3({
-  /*api version etc.. here */
-});
-
-// create a name for your bucket. Refer to https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html for naming rules.
-const bucketName = 'my-bucket';
-
-// set the parameters for the bucket you are creating
+// Set the parameters for the bucket creation
 const bucketParams = {
   Bucket: bucketName,
-  /* other parameters check refer to https://docs.aws.amazon.com/AmazonS3/latest/userguide/creating-buckets-s3.html */
+  // Additional parameters can be added here
 };
 
-// function to create a new bucket
-const createBucket = async (bucketName, bucketParams) => {
+// Async function to create a new bucket
+const createBucket = async () => {
   try {
-    // create a variable to store a promise that will be resolved when the uploadData is successfully in the bucket
-    const uploadData = await s3.createBucket(bucketParams).promise();
+    // Attempt to get the bucket to check if it exists
+    await s3.headBucket({ Bucket: bucketName }).promise();
+    console.log(`Bucket ${bucketName} already exists.`);
   } catch (err) {
-    console.error(`Error creating bucket: ${err.message}`);
+    if (err.statusCode === 404) {
+      // Bucket does not exist, so create it
+      await s3.createBucket(bucketParams).promise();
+      console.log(`Bucket ${bucketName} created successfully.`);
+    } else {
+      // An error occurred (e.g., network error, invalid credentials)
+      throw err;
+    }
   }
 };
 
-// call the function to create the bucket
-createBucket(bucketName, bucketParams);
+// Call the function to create the bucket
+createBucket().catch((err) => {
+  console.error(`Error creating bucket: ${err.message}`);
+});
